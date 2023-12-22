f00d17cce835a8c5d44eddc7bce578d4ddc82459
drpsych <62787554+drpsych@users.noreply.github.com>
2023-12-21 22:19:25 -0500

    added SplideSlide for popular items with styles
    styled the slide component for popular items.

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import "./components.css";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
 function Popular() {
     const [popular, setPopular] = useState([]);
@@ -10,7 +12,7 @@ function Popular() {
 
     const getPopular = async () => {
         const api = await fetch(
-            `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=3`
+            `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=8`
         );
         const data = await api.json();
         console.log(data);
@@ -20,14 +22,27 @@ function Popular() {
     return (
         <div className="wrapper">
             <h3>Popular Picks</h3>
-            {popular.map((recipe) => {
-                return (
-                    <div className="card" key={recipe.id}>
-                        <p>{recipe.title}</p>
-                        <img src={recipe.image} alt={recipe.title} />
-                    </div>
-                );
-            })}
+            <Splide
+                options={{
+                    perPage: 4,
+                    arrows: false,
+                    pagination: false,
+                    drag: "free",
+                    gap: "5rem",
+                }}
+            >
+                {popular.map((recipe) => {
+                    return (
+                        <SplideSlide>
+                            <div className="card" key={recipe.id}>
+                                <p>{recipe.title}</p>
+                                <img src={recipe.image} alt={recipe.title} />
+                                <div className="gradient"></div>
+                            </div>
+                        </SplideSlide>
+                    );
+                })}
+            </Splide>
         </div>
     );
 }
