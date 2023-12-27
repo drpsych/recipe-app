92e92e1027b65b4791b49514dc639093e7dfb779
drpsych <62787554+drpsych@users.noreply.github.com>
2023-12-27 18:39:52 -0500

    routing for cuisine started

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,25 +1,26 @@
 import { FaHamburger, FaPizzaSlice } from "react-icons/fa";
 import { GiTacos, GiChopsticks } from "react-icons/gi";
+import { NavLink } from "react-router-dom";
 
 function Category() {
     return (
         <div className="list">
-            <div>
+            <NavLink to={"cuisine/Italian"}>
                 <FaPizzaSlice />
                 <h4>Italian</h4>
-            </div>
-            <div>
+            </NavLink>
+            <NavLink to={"cuisine/American"}>
                 <FaHamburger />
                 <h4>American</h4>
-            </div>
-            <div>
+            </NavLink>
+            <NavLink to={"cuisine/Mexican"}>
                 <GiTacos />
                 <h4>Mexican</h4>
-            </div>
-            <div>
+            </NavLink>
+            <NavLink to={"cuisine/Asian"}>
                 <GiChopsticks />
                 <h4>Asian</h4>
-            </div>
+            </NavLink>
         </div>
     );
 }
