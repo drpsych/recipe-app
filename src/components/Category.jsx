c9d521150e7152831cebf16bbdef7328875c331b
drpsych <62787554+drpsych@users.noreply.github.com>
2023-12-27 20:43:55 -0500

    working category routing with api calls and some styles

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -5,19 +5,19 @@ import { NavLink } from "react-router-dom";
 function Category() {
     return (
         <div className="list">
-            <NavLink to={"cuisine/Italian"}>
+            <NavLink to={"cuisine/Italian"} className="cat-link">
                 <FaPizzaSlice />
                 <h4>Italian</h4>
             </NavLink>
-            <NavLink to={"cuisine/American"}>
+            <NavLink to={"cuisine/American"} className="cat-link">
                 <FaHamburger />
                 <h4>American</h4>
             </NavLink>
-            <NavLink to={"cuisine/Mexican"}>
+            <NavLink to={"cuisine/Mexican"} className="cat-link">
                 <GiTacos />
                 <h4>Mexican</h4>
             </NavLink>
-            <NavLink to={"cuisine/Asian"}>
+            <NavLink to={"cuisine/Asian"} className="cat-link">
                 <GiChopsticks />
                 <h4>Asian</h4>
             </NavLink>
--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import "./pages.css";
 
 function Cuisine() {
-    return <div>Cuisine</div>;
+    const [cuisine, setCuisine] = useState([]);
+    let params = useParams();
+
+    const getCuisine = async (name) => {
+        const data = await fetch(
+            `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}&number=10`
+        );
+        const recipes = await data.json();
+        setCuisine(recipes.results);
+        console.log(recipes.results);
+    };
+
+    useEffect(() => {
+        getCuisine(params.type);
+        console.log(params.type);
+    }, [params.type]);
+
+    return (
+        <div className="grid">
+            {cuisine.map((item) => {
+                return (
+                    <div className="recipe-card" key={item.id}>
+                        <img src={item.image} alt={item.title} />
+                        <h4>{item.title}</h4>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
 
 export default Cuisine;
