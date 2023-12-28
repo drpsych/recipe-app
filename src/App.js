a9fb4f629467472c9d24f23e835328c8868c4427
drpsych <62787554+drpsych@users.noreply.github.com>
2023-12-28 18:32:23 -0500

    logo added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Category from "./components/Category";
 import Search from "./components/Search";
 import Pages from "./pages/Pages";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
+import { GiKnifeFork } from "react-icons/gi";
 
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
+                <div className="nav">
+                    <GiKnifeFork />
+                    <Link className="logo" to={"/"}>
+                        delicious
+                    </Link>
+                </div>
                 <Search />
                 <Category />
                 <Pages />
