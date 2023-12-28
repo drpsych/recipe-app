8c0874b3cb819492b6a97cb8b6ceca76846c6846
drpsych <62787554+drpsych@users.noreply.github.com>
2023-12-27 21:42:19 -0500

    Search by keyword implemented (#4) (#5)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Category from "./components/Category";
+import Search from "./components/Search";
 import Pages from "./pages/Pages";
 import { BrowserRouter } from "react-router-dom";
 
@@ -6,6 +7,7 @@ function App() {
     return (
         <div className="App">
             <BrowserRouter>
+                <Search />
                 <Category />
                 <Pages />
             </BrowserRouter>
